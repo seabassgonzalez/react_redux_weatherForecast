224b25a9e0baa28482fd819f97318c1d0fe85137
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 14:52:35 -0800

    Initial src directory and index.js root file, pseudocode for using react redux and middleware settings

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// import React from react
+// import ReactDOM from react-dom
+// import Provider from react-redux
+// import createStore and applyMiddleware from redux
+// import App
+// import reducers
+
+// create const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+// ReactDOM.render
+	// Provider store object - createStoreWithMiddleware(reducers)
+		// App Component
+	// at DOM container component
