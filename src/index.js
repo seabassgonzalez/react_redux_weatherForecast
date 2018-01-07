e64f5383468fd5851f06281a4540cfe728f367f0
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 14:54:50 -0800

    src root index.js implementation of react redux middleware, initial files for actions components and reducers

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@
 	// Provider store object - createStoreWithMiddleware(reducers)
 		// App Component
 	// at DOM container component
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
