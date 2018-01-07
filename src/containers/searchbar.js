0c00b8bec9587b669f90bd191807d7b343f23b78
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 15:17:05 -0800

    Initial searchbar component pseudocode importing libraries and exporting class

---
+++ src/containers/searchbar.js
@@ -0,0 +1,7 @@
+// import React and Component from react
+
+// export default class Searchbar extending component
+	// render
+		// return
+			// a form with className input-group
+				// input component
