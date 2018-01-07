949fd95aec48596c5de9c6a2e73109491062cae9
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 15:00:56 -0800

    Initial root reducer implementation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,3 +4,11 @@
 	// state set to initial object
 
 // export rootReducer
+
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  state: (state = {}) => state
+});
+
+export default rootReducer;
