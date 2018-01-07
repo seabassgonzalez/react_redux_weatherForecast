8a051a3e16e5b65dceba0a43a9e30089babb4642
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 15:07:32 -0800

    Initial implementation of app component hello world div

--- src/components/app.js
+++ src/components/app.js
@@ -4,3 +4,13 @@
 	// render
 		// return
 			// div hello world
+
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	render(){
+		return(
+			<div>Hello weather forecasting world!</div>
+		);
+	}
+}
