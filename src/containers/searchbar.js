046bc5d2d794d7352c243d5d97923616ab30ab8b
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 15:50:57 -0800

    Bind instance of onInputChange

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -27,6 +27,7 @@ export default class SearchBar extends Component {
 	constructor(props){
 		super(props);
 		this.state = { term: '' };
+		this.onInputChange = this.onInputChange.bind(this);
 	}
 
 	onInputChange(event){
