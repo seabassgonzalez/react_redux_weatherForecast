4ab30c1242c70fc4e1968b102e5bb9a49fac8025
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 15:33:46 -0800

    Implement initial state for searchbar container, an empty term string

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -15,6 +15,11 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props){
+		super(props);
+		this.state = { term: '' };
+	}
+
 	render(){
 		return(
 			<form className='input-group'>
