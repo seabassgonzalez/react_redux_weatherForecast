5c5c7311703d29fc827363c2d836c33d97f86011
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 16:58:48 -0800

    Save root url as a const

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // save api key as const
+// save root URL as const
 
 // export const FETCH_WEATHER type as string to keep action types consistent between action creators and reducers
 
@@ -7,6 +8,7 @@
 		// type set to fetch weather variable
 		
 const API_KEY='8b4f27dea26e093bf2ddbb6bfea22ad6';
+const ROOT_URL='http://api.openweathermap.org/data/2.5/forecast?';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
