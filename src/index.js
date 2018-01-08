02c7fb8ecdd189ec0afb13fa3de093dc6c74589a
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 16:46:38 -0800

    Import and use reduxpromise

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 // import ReactDOM from react-dom
 // import Provider from react-redux
 // import createStore and applyMiddleware from redux
+// import reduxpromise from redux-promise
 // import App
 // import reducers
 
-// create const createStoreWithMiddleware = applyMiddleware()(createStore)
+// create const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 // ReactDOM.render
 	// Provider store object - createStoreWithMiddleware(reducers)
@@ -16,11 +17,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
