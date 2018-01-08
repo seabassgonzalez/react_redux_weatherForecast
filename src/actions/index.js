0d9eef484b8c0cc166ea1fe65968991e32767891
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 16:54:00 -0800

    Export fetch weather function while extrapolating action creator type as a variable first, will import that variable into the reducer as well and will be less prone to mistakes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
 // save api key as const
 
+// export const FETCH_WEATHER type as string to keep action types consistent between action creators and reducers
+
+// export function fetchWeather action creator
+	// return
+		// type set to fetch weather variable
+		
 const API_KEY='8b4f27dea26e093bf2ddbb6bfea22ad6';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(){
+	return{
+		type: FETCH_WEATHER
+	};
+}
