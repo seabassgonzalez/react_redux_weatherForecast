931ee127153911888cdbc51aa5541e5dd7d586b1
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-07 17:01:31 -0800

    Use ES6 to concatenate root url and api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,7 +8,7 @@
 		// type set to fetch weather variable
 		
 const API_KEY='8b4f27dea26e093bf2ddbb6bfea22ad6';
-const ROOT_URL='http://api.openweathermap.org/data/2.5/forecast?';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
