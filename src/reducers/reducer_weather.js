b73d197e07a5115057f312abff2e87d5a280deb1
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:01:59 -0800

    Implement simple reducer for fetchweather action, returns state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,8 @@
 // export reducer - function that take particular state its responsible for, set to null, and an action
 	// console.log action received for testing
 	// return state
+
+export default function(state = null, action){
+	console.log('Action received: ', action);
+	return state;
+}
