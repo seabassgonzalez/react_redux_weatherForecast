fe09b639bb71968b57476da90ed8c5c16fbe9269
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:38:43 -0800

    Simple table scaffolding for data result array, WeatherList class extending Component rendering and returning

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -8,3 +8,21 @@
 					// tr
 						// th City
 				// tbody
+
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component{
+	render(){
+		return (
+			<table>
+				<thead>
+					<tr>
+						<th>City</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		);
+	}
+}
