46c0564874f4bba60fd8e77196fae430efe0a87f
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:36:12 -0800

    Basic scaffolding for a table to hold city results array, accessing data from redux so container

---
+++ src/containers/weather_list.js
@@ -0,0 +1,10 @@
+// import React and Component from react
+
+// export default class WeatherList extending Component
+	// render
+		// return
+			// table
+				// table head
+					// tr
+						// th City
+				// tbody
