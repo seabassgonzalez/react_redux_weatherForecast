4f3cbc229da35e65c12070f6fc2d0b278f87a9b6
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:24:33 -0800

    Import FETCH_WEATHER action, pseudocode for switch statement to handle action, particularly creating a new instance of state with each search, concatenating data in array accumulated each time

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,8 +1,13 @@
-// export reducer - function that take particular state its responsible for, set to null, and an action
+// import FETCH_WEATHER '../actions/index'
+
+// export reducer - function that take particular state its responsible for, set to an empty array because pieces of weather data should be stored as an array, and an action
 	// console.log action received for testing
+	// setup a switch statement to handle action
+		// case FETCH_WEATHER
+			// handle payload -- return a new instance of state - -array that accumulates weather data for many searches -- state is an array, concat action.payload.data array onto it
 	// return state
 
-export default function(state = null, action){
+export default function(state = [], action){
 	console.log('Action received: ', action);
 	return state;
 }
