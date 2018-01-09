b3576c8ef05d505cfb816c8d2a9f68a57e20882b
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:29:15 -0800

    Implement switch statement to handle action and data, using es6 syntactical sugar to concatenate each search result into new search result array

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,9 +5,17 @@
 	// setup a switch statement to handle action
 		// case FETCH_WEATHER
 			// handle payload -- return a new instance of state - -array that accumulates weather data for many searches -- state is an array, concat action.payload.data array onto it
+			// es6 syntax sugar shows can be equivalent return [ action.payload.data, ...state ] -- will also return [city, city, city]
 	// return state
+
+import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action){
 	console.log('Action received: ', action);
+	switch (action.type){
+		case FETCH_WEATHER:
+			// return state.concat([action.payload.data]);
+			return [ action.payload.data, ...state ];
+	}
 	return state;
 }
