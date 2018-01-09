a2a2c44fdab906cd927c0854b015ce4bf9df5775
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-08 22:42:59 -0800

    Import WeatherList and render in div

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
 // import React and Component from react
-// import SearchBar from './src/containers/searchbar'
+// import SearchBar from '../containers/searchbar'
+// import WeatherList from '../containers/weather_list'
 
 // export App class extending Component
 	// render
 		// return
 			// div 
 				// SearchBar component
+				// WeatherList component
 
 import React, { Component } from 'react';
 import SearchBar from '../containers/searchbar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
 	render(){
