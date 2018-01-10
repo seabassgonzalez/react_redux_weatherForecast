8b73f18e6f51779d2b740ad2d77ce336ccc6f469
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-09 22:09:41 -0800

    Implement skeleton for GoogleMap component

---
+++ src/components/google_map.js
@@ -0,0 +1,13 @@
+// import React and Component from react
+
+// class GoogleMap extends Component
+
+// export default GoogleMap
+
+import React, { Component } from 'react';
+
+class GoogleMap extends Component{
+
+}
+
+export default GoogleMap;
