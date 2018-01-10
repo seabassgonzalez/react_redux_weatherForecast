3c6e9bb3271e7a74c92c36d5ab277e3d328745aa
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-09 22:27:09 -0800

    Use es6 syntax to condense code

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -52,8 +52,7 @@ class WeatherList extends Component {
 		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp => temp - 273));
 		const pressures = cityData.list.map(weather => weather.main.pressure);
 		const humidities = cityData.list.map(weather => weather.main.humidity);
-		const lon = cityData.city.coord.lon;
-		const lat = cityData.city.coord.lat;
+		const { lon, lat } = cityData.city.coord;
 
 		console.log(temps);
 
