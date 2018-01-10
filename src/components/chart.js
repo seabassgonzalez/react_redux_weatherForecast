4286519619c68b2bb599c1eae0e0b754c7cf2d8e
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-09 21:21:00 -0800

    Implement Sparklines chart component, simple div

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,7 +1,19 @@
 // dont need to track of component state, just props come in and render a component
 
 // import React from react
+// import Sparklines and SparklinesLine from react-sparklines
 
 // export default (props) function
 	// return
 		// div
+
+import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
+export default (props)=>{
+	return(
+		<div>
+
+		</div>
+	);
+}
