aeb0ca47af9cb71d576d4e35e6cbd2c4f2de256e
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-09 21:48:05 -0800

    Defined helper function average to calculate average using lodash

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,6 +2,11 @@
 
 // import React from react
 // import Sparklines, SparklinesLine, and SparklinesReferenceLine from react-sparklines
+// import _ from lodash to calculate average
+
+// create function to calculate average, taking data
+	// return
+		// sum of data divided by length of data = average, round it off also
 
 // export default (props) function
 	// return
@@ -9,9 +14,16 @@
 			// Sparklines height width and data set to array of temps, but no access to temps anymore to will access as props.data
 				// SparklinesLine setting code as {props.color} to change at will
 				// SparklinesReferenceLine type of avg for average
+				// div
+					// call helper function average passing array of numbers into it with props.data
 
 import React from 'react';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data){
+	return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
 	return(
