e99f5a4334216dbe962d0a6c4e2b1f8bbae00b3c
seabassgonzalez <seabassgonzalez@gmail.com>
2018-01-09 22:14:47 -0800

    Initial pseudocode following googlemaps api specifications for componentdidMount method, and render method

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,7 +1,14 @@
 // import React and Component from react
 
 // class GoogleMap extends Component
-
+	// componentDidMount
+		// new google.maps.Map() taking this.refs.map and an object with two properties
+			// zoom 12
+			// center
+				// lat this.props.lat
+				// long this.props.long
+	// render
+		// return div with prop ref="map"
 // export default GoogleMap
 
 import React, { Component } from 'react';
